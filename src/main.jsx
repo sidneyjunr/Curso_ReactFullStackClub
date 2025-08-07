3e0009cee426416c5aa01ca43151835a4d4f9811
Sidney <sidneyferreirabaptistajunior3@gmail.com>
2025-08-07 14:48:49 -0300

    ajuste no browseRouter para hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { TaskPage } from "./pages/TaskPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element ={<App/>}/>
         <Route path="/task" element ={<TaskPage/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
