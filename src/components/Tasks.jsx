e70498648df5f8c926b11b838e01512f46cadc77
Sidney <sidneyferreirabaptistajunior3@gmail.com>
2025-08-07 14:53:37 -0300

    Ajustes simples no index.html e no tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -17,10 +17,11 @@ export const Tasks = ({ tasks, onDeleteTaskClick, onTaskClick }) => {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon/>}
             {task.title}
           </button>
           <button
